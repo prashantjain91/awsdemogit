ae284258f7746d7fad9a2902666f61646dc8b2c9
unknown <Admin@EMLP-MUM0016.aurum.com>
2021-01-18 18:00:41 +0530

    new content added to app.js file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This is the new content to check the git repo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
